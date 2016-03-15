6bf91ffe249992a17d2f0e115fa5c143aaa0a223
Nicolas Corrarello <ncorrare@gmail.com>
2016-03-15 22:09:22 +0000

    Added space to square bracket

--- aftersuccessforge.sh
+++ aftersuccessforge.sh
@@ -8,7 +8,7 @@ export GIT_COMMITTER_EMAIL='travis@travis'
 export GIT_COMMITTER_NAME='Travis CI'
 
 # I just want to do one merge, so I'm checking I'm running in the right Travis Job (Environment)
-if [ "$PUPPET_GEM_VERSION" = "~> 4.3"]; then
+if [ "$PUPPET_GEM_VERSION" = "~> 4.3" ]; then
 	if ! grep -q "$BRANCHES_TO_MERGE_REGEX" <<< "$TRAVIS_BRANCH"; then
 	    	printf "Current branch %s doesn't match regex %s, exiting\\n" \
 		            "$TRAVIS_BRANCH" "$BRANCHES_TO_MERGE_REGEX" >&2
