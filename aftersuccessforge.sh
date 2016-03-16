48190b7bf15eb2b772fb4b7d62729a41fbfa2acf
Nicolas Corrarello <ncorrare@gmail.com>
2016-03-16 21:00:44 +0000

    Fix gem version;

--- aftersuccessforge.sh
+++ aftersuccessforge.sh
@@ -5,11 +5,15 @@
 
 export GIT_COMMITTER_EMAIL='travis@travis'
 export GIT_COMMITTER_NAME='Travis CI'
+export BLACKSMITH_FORGE_URL
+export BLACKSMITH_FORGE_USERNAME
+export BLACKSMITH_FORGE_PASSWORD
+
 
 export SHA=$(curl https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST |jq -r .head.sha)
 export MESSAGE="Merged automatically by Travis CI. Full testing details on https://travis-ci.org/$TRAVIS_REPO_SLUG/jobs/$TRAVIS_BUILD_ID"
 # I just want to do one merge, so I'm checking I'm running in the right Travis Job (Environment)
-if [ "$PUPPET_GEM_VERSION" = "~> 4.3" ]; then
+if [ "$PUPPET_GEM_VERSION" = "= 4.3" ]; then
 	if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	    	printf "This is not a Pull Request, I won't be deploying this. Exiting\\n"
 	        exit 0
@@ -21,10 +25,6 @@ if [ "$PUPPET_GEM_VERSION" = "~> 4.3" ]; then
 	printf "Merge status is $MERGE"
 	if [ $MERGE = true ]; then
 		printf "go and deploy stuff"
-		echo "---
-		url: $BLACKSMITH_FORGE_URL
-		username: $BLACKSMITH_FORGE_USERNAME
-		password: $BLACKSMITH_FORGE_PASSWORD" > ~/.puppetforge.yml
 		repo_temp=$(mktemp -d)
 		cd "$repo_temp"
 		git clone https://$GITHUB_SECRET_TOKEK@github.com/$TRAVIS_REPO_SLUG.git "$repo_temp"
