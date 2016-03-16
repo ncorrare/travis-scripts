ee4e55a262ef2bcab067ca25e0e9bb884374d0a3
Nicolas Corrarello <ncorrare@gmail.com>
2016-03-16 20:13:16 +0000

    Added message for merge status

--- aftersuccessforge.sh
+++ aftersuccessforge.sh
@@ -15,6 +15,8 @@ if [ "$PUPPET_GEM_VERSION" = "~> 4.3" ]; then
 	        exit 0
 	fi
 
+	printf "SHA of the Commit is $SHA\n"
+	printf "Merging PR# $TRAVIS_PULL_REQUEST with message $MESSAGE\n"
 	MERGE=$(curl -X PUT --data "{\"commit_message\":\"$MESSAGE\",\"sha\":\"$SHA\"}" https://$GITHUB_SECRET_TOKEN@api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/merge | jq -r .merged)
 	printf "Merge status is $MERGE"
 	if [ $MERGE = true ]; then
