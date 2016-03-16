8f08c7f6b55d957fc6d0b3879804677f2492f7ea
Nicolas Corrarello <ncorrare@gmail.com>
2016-03-16 20:31:41 +0000

    Release workflow complete

--- aftersuccessforge.sh
+++ aftersuccessforge.sh
@@ -21,30 +21,9 @@ if [ "$PUPPET_GEM_VERSION" = "~> 4.3" ]; then
 	printf "Merge status is $MERGE"
 	if [ $MERGE = true ]; then
 		printf "go and deploy stuff"
+		repo_temp=$(mktemp -d)
+		cd "$repo_temp"
+		git clone https://$GITHUB_SECRET_TOKEK@github.com/$TRAVIS_REPO_SLUG.git "$repo_temp"
+		rake module:release
 	fi
-	# Since Travis does a partial checkout, we need to get the whole thing
-	#repo_temp=$(mktemp -d)
-	#git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
-
-	# shellcheck disable=SC2164
-	#cd "$repo_temp"
-
-	#printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
-	#git checkout "$BRANCH_TO_MERGE_INTO"
-
-	#printf 'Merging %s\n' "$TRAVIS_COMMIT" >&2
-	#git merge --ff-only "$TRAVIS_COMMIT"
-
-	#printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
-
-	#push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
-
-	# Redirect to /dev/null to avoid secret leakage
-	#git push "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
-	#git push "$push_uri" :"$TRAVIS_BRANCH" >/dev/null 2>&1
-	
-	#if [ $PUPPET_GEM_VERSION = "~> 4.3" ] && [ $DEPLOY = "true" ]; then
-	#	printf "Pushing module to the Puppet Forge\n"
-	#	rake module:release
-	#fi
 fi
